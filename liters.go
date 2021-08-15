420f74e16a469d91c3cce9b6b321c619bd7b7359
lazycruise <gatesworkline@gmail.com>
2021-08-15 01:03:34 +0100

    update functional go code

--- liters.go
+++ liters.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 )
-
+func sayHi()  {
+	fmt.Println("hi!")
+}
 func main() {
 	var width, length, area float64
 	width = 3.45
 	length = 3.22
 	area = width * length
-	fmt.Println(area/10.00, "liters of paint is needed.")
+	fmt.Printf("%.2f liters of paint is needed.\n", area/10.0)
+	width = 5.22
+	length = 3.5
+	area = width * length
+	fmt.Printf("%.2f liters of paint is needed.\n", area/10.0)
+	sayHi()
 }
