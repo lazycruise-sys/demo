d338f68f0fc6cd21c14cd392376377a9e7eb084a
lazycruise <gatesworkline@gmail.com>
2021-08-23 07:28:06 +0100

    update function code

--- liters.go
+++ liters.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 )
 
-func paintNeeded(width float64, length float64) {
+var metersPerLiter float64
+
+func paintNeeded(width , length float64) float64 {
 	area := width * length
-	fmt.Printf("%.2f liters of paint is needed.\n", area/10.0)
+	return area/metersPerLiter
 }
 
 func main() {
-	paintNeeded(5.2, 3.4)
+	metersPerLiter = 10.0
+	fmt.Printf("%.2f", paintNeeded(5.2, 3.4))
 	paintNeeded(3.4, 7.0)
 	paintNeeded(6.7, 3.4)
 }
