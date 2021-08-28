8384244a1e9fc80d981518458c2123d2b1e5c313
lazycruise <gatesworkline@gmail.com>
2021-08-28 23:41:35 +0100

    update array literals

--- literal.go
+++ literal.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// initializing array
@@ -9,4 +12,20 @@ func main() {
 	notes[1] = "re"
 	notes[2] = "mi"
 	fmt.Println(notes)
+
+
+	var dates [3] time.Time
+	dates[0] = time.Unix(3289239010, 0)
+	fmt.Println(dates[0])
+
+	var primes [5] int
+	primes[0] = 2
+	primes[1] = 3
+	primes[2] = 5
+	fmt.Println(primes)
+	primes[1]++
+	fmt.Println(primes)
+
+	names := [3] string {"akinlua", "olorunfemi", "praise"}
+	fmt.Println(names)
 }
