302b58eb2b50ab9f903afb1fcb0afc9808b6e424
lazycruise <gatesworkline@gmail.com>
2021-09-25 22:46:09 +0100

    show go defined types operations

--- type.go
+++ type.go
@@ -8,11 +8,20 @@ import (
 // defined type declaration
 type Liters float64
 type Gallons float64
+type Milliliters float64
 type Title string
 type Car struct {
 	Name       string
 	TopSpeed   float64
 	EngineType string
+}
+
+func ToLiters(g Gallons) Liters {
+	return Liters(g * 3.785)
+}
+
+func ToGallons(l Liters) Gallons {
+	return Gallons(l * 0.264)
 }
 
 func main() {
@@ -59,4 +68,23 @@ func main() {
 	fmt.Println(Title("allen") == Title("allen"))
 	fmt.Println(Title("allen") > Title("zodiac"))
 
+	// defined types can be used in operations
+	// together with literal values
+	fmt.Println(Liters(1.2) + 3.4)
+	fmt.Println(Gallons(5.5) - 2.2)
+	checkCalcType := Gallons(5.6) + 2.4 // has a main.Gallons type
+	fmt.Println(reflect.TypeOf(checkCalcType))
+	fmt.Println(Gallons(1.2) == 1.2)
+	// however, they can't be used in operations
+	// with values of different types such as
+	// Gallons(1.2) + Liter(23.4)
+	// Gallons(2.3) > Liters(2.2)
+
+
+	// converting between types using functions
+	carFuel, busFuel= Gallons(1.2), Liters(4.5)
+	carFuel += ToGallons(Liters(40.0))
+	busFuel += ToLiters(Gallons(30.0))
+	fmt.Printf("car fuel: %.2f gallons.\n", carFuel)
+	fmt.Printf("bus fuel: %.2f liters.\n", busFuel)
 }
